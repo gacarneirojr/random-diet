84995388de59502d02ab438b868d1c333963a2ad
Gabriel Carneiro <71238933+gacarneirojr@users.noreply.github.com>
2020-12-14 11:08:04 -0300

    -Readme.md updated -Comments on main.js -index.html created for test

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 
 ## This program gives a full day healthy diet for you
 
-*Based on JavaScript, this program will generate a 3 meals pics with healthy foods randomly
+**Based on JavaScript, this program will generate a 3 meals pics with healthy foods randomly**
+
+**There are 18 different combinations available in this Diet Generator**
+
+**Only JavaScript is used. No frameworks**
+
+*This is a project exercise required for partial fulfillment of CodeCademy JavaScript course
+
+
 
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Those are samples of regular meals of the day
 const breakfast =   [
     ["coffee with milk, 2 eggs, 1 toast, 1 cereal bar"],
     ["orange juice, 2 slices of bacon, 2 slices of bread, 1 apple, yogurt"],
@@ -16,13 +17,16 @@ const dinner = [
     ["spaghetti, shrimp, sauce"]
 ];
 
-
+//This function pics 3 random numbers to be used for each meal of the day. The result returns a message
+// with 18 random possibilities of meals combinations
 const dietGenerator = () => {
-    let random1 = Math.floor(Math.random()*4);
-    let random2 = Math.floor(Math.random()*4);
-    let random3 = Math.floor(Math.random()*4);
+    let random1 = Math.floor(Math.random()*3);
+    let random2 = Math.floor(Math.random()*3);
+    let random3 = Math.floor(Math.random()*3);
     let breakfastR = breakfast[random1];
     let lunchR = lunch[random2];
     let dinnerR = dinner[random3];
     return `Your today's diet is ${breakfastR} for breakfast, ${lunchR} for lunch, and ${dinnerR} for dinner`
 }
+
+console.log(dietGenerator());
