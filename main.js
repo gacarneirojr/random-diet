ad1594e189f7190436d722630f80d356734c859c
Gabriel Carneiro <71238933+gacarneirojr@users.noreply.github.com>
2020-12-14 11:48:19 -0300

    -Variables inside dietGenerator() for random pics removed. -Function random() created and implemented in dietGenerator() to improve the code as suggested in the forum.

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 //Those are samples of regular meals of the day
 const breakfast =   [
-    ["coffee with milk, 2 eggs, 1 toast, 1 cereal bar"],
-    ["orange juice, 2 slices of bacon, 2 slices of bread, 1 apple, yogurt"],
-    ["tea, 1 biscuit, 2 cream crackers, pear fruit, whey protein"]
+    ["coffee with milk, 2 eggs, 1 toast, and 1 cereal bar"],
+    ["orange juice, 2 slices of bacon, 2 slices of bread, 1 apple, and yogurt"],
+    ["tea, 1 biscuit, 2 cream crackers, pear fruit, and whey protein"]
     ];
 
 const lunch = [
     ["rice, beans, meat, and lettuce"],
     ["spaghetti, meatballs, egg salad"],
-    ["risotto, beacon, caesar salad"]
+    ["risotto, beacon, and caesar salad"]
 ];
 
 const dinner = [
-    ["chicken, salad, bread"],
-    ["hamburger, salad, bread"],
-    ["spaghetti, shrimp, sauce"]
+    ["chicken, salad, and bread"],
+    ["hamburger, salad, and bread"],
+    ["spaghetti, shrimp, and sauce"]
 ];
+
+//Generates a random number according to the size of the meal arrays.
+const random  = (meal) => {
+    return Math.floor(Math.random()*meal.length);
+}
 
 //This function pics 3 random numbers to be used for each meal of the day. The result returns a message
 // with 18 random possibilities of meals combinations
 const dietGenerator = () => {
-    let random1 = Math.floor(Math.random()*breakfast.length);
-    let random2 = Math.floor(Math.random()*lunch.length);
-    let random3 = Math.floor(Math.random()*dinner.length);
-    let breakfastR = breakfast[random1];
-    let lunchR = lunch[random2];
-    let dinnerR = dinner[random3];
+    let breakfastR = breakfast[random(breakfast)];
+    let lunchR = lunch[random(lunch)];
+    let dinnerR = dinner[random(dinner)];
     return `Your today's diet is ${breakfastR} for breakfast, ${lunchR} for lunch, and ${dinnerR} for dinner`
 }
 
